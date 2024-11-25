9361b92dac198026fa21833b5500fa30b6f16cd9
Xalasca <diovani@anticasper.com>
2024-11-25 19:17:54 -0300

    test: create Domain\AccountManager and set on test

--- tests/AccountManagerTest.php
+++ tests/AccountManagerTest.php
@@ -3,8 +3,16 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use App\Domain\AccountManager;
 
 class AccountManagerTest extends TestCase {
+
+    private AccountManager $manager;
+
+    protected function setUp(): void
+    {
+        $this->manager = new AccountManager();
+    }
 
     public function testReset(): void{
         
