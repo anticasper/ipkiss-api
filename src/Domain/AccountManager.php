94a1f7f137587187fc2e300e99de34ab00f33871
Xalasca <diovani@anticasper.com>
2024-11-25 20:00:57 -0300

    feat: create account function

--- src/Domain/AccountManager.php
+++ src/Domain/AccountManager.php
@@ -3,5 +3,27 @@
 namespace App\Domain;
 
 class AccountManager {
-    
+
+    private array $accounts = [];
+
+    public function createAccount(string $accountId, int $initialBalance): array
+    {
+        if (!isset($this->accounts[$accountId])) {
+            $this->accounts[$accountId] = ['id' => $accountId, 'balance' => $initialBalance];
+        } else {
+            $this->accounts[$accountId]['balance'] += $initialBalance;
+        }
+        return $this->accounts[$accountId];
+    }
+
+    public function reset(): void
+    {
+        
+    }
+
+    public function getBalance(string $accountId): ?int
+    {
+        
+    }
+
 }
