ff3d1229b4369e8cfd399034206a72bad0624e56
Xalasca <diovani@anticasper.com>
2024-11-25 21:13:52 -0300

    feat: add manager functions

--- src/Application/AccountService.php
+++ src/Application/AccountService.php
@@ -13,4 +13,29 @@ class AccountService
         $this->manager = $manager;
     }
 
+    public function resetState(): void
+    {
+        $this->manager->reset();
+    }
+
+    public function getBalance(string $accountId): ?int
+    {
+        return $this->manager->getBalance($accountId);
+    }
+
+    public function deposit(string $accountId, int $amount): array
+    {
+        return $this->manager->manageAccount($accountId, $amount);
+    }
+
+    public function withdraw(string $accountId, int $amount): ?array
+    {
+        return $this->manager->withdraw($accountId, $amount);
+    }
+
+    public function transfer(string $originId, string $destinationId, int $amount): ?array
+    {
+        return $this->manager->transfer($originId, $destinationId, $amount);
+    }
+
 }
