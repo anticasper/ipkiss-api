556cd4d5d5b71626ef8183ef26524e35e1f092da
Xalasca <diovani@anticasper.com>
2024-11-25 21:11:05 -0300

    feat: construct class

--- src/Application/AccountService.php
+++ src/Application/AccountService.php
@@ -2,7 +2,15 @@
 
 namespace App\Application;
 
+use App\Domain\AccountManager;
+
 class AccountService
 {
+    private AccountManager $manager;
+
+    public function __construct(AccountManager $manager)
+    {
+        $this->manager = $manager;
+    }
 
 }
