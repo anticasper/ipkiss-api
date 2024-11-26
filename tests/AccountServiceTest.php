96b85b32d1d6ce6eb4a54273782ae64fa7a05daf
Xalasca <diovani@anticasper.com>
2024-11-25 21:09:13 -0300

    test: setup account service

--- tests/AccountServiceTest.php
+++ tests/AccountServiceTest.php
@@ -3,8 +3,18 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use App\Domain\AccountManager;
+use App\Application\AccountService;
 
 class AccountServiceTest extends TestCase {
+
+    private AccountService $service;
+
+    protected function setUp(): void
+    {
+        $manager = new AccountManager();
+        $this->service = new AccountService($manager);
+    }
 
 }
 
